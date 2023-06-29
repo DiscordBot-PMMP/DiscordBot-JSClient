95669985dc558634d4b42607d67a05e51595b46f
Jack Honour <JaxkDev@gmail.com>
2023-06-30 00:05:01 +0100

    Trial data length prefix

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const port = 22222;
 
 const client = net.createConnection(port, host, () => {
     console.log("Connected");
-    client.write(JSON.stringify({"status": 0, "packet": [0, {"version": 1, "magic": 0x4a61786b446576}]}))
+    const data = JSON.stringify({"status": 0, "packet": [0, {"version": 1, "magic": 0x4a61786b446576}]})
+    const buf = Buffer.alloc(4 + Buffer.byteLength(data, "utf8"));
+    buf.writeUInt32BE(Buffer.byteLength(data, "utf8"));
+    buf.write(data, 4)
+    client.write(buf);
 });
 
 client.on("data", (data) => {
-    console.log(`Received: ${JSON.stringify(JSON.parse(data.toString()))}`);
+    const length = data.readUInt32BE(0);
+    data = data.subarray(4, length + 4);
+    console.log(`Received: Len=${length} Data=${data.toString()}`);
 });
 
 client.on("error", (error) => {
