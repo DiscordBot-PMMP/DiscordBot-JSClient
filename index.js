b7885a72926a17ac33a58b4df0041bc89489e8c7
Jack Honour <JaxkDev@gmail.com>
2023-06-30 01:53:29 +0100

    Small tool functions

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const net = require("net");
 const host = "127.0.0.1";
 const port = 22222;
 
-const client = net.createConnection(port, host, () => {
-    console.log("Connected");
-    const data = JSON.stringify({"status": 0, "packet": [0, {"version": 1, "magic": 0x4a61786b446576}]})
-    const buf = Buffer.alloc(4 + Buffer.byteLength(data, "utf8"));
+function getBuffer(data) {
+    data = JSON.stringify(data);
+    let buf = Buffer.alloc(4 + Buffer.byteLength(data, "utf8"));
     buf.writeUInt32BE(Buffer.byteLength(data, "utf8"));
     buf.write(data, 4)
-    client.write(buf);
+    return buf;
+}
+
+const client = net.createConnection(port, host, () => {
+    console.log("Connected, sending verify packet.");
+    client.write(getBuffer([0, {"version": 1, "magic": 0x4a61786b446576}]))
 });
 
 client.on("data", (data) => {
     const length = data.readUInt32BE(0);
-    data = data.subarray(4, length + 4);
-    console.log(`Received: Len=${length} Data=${data.toString()}`);
+    let cdata = data.subarray(4, length + 4);
+    data = data.subarray(length + 4);
+    if(data.length > 0){
+        console.log("leftover data: " + data);
+    }
+    console.log(`Received: Len=${length} Data=${cdata.toString()}`);
 });
 
 client.on("error", (error) => {
@@ -35,7 +43,7 @@ process.on("exit", async() => {
 
 process.on('SIGINT', function() {
     console.error('Emergency exit.');
-    client.write(JSON.stringify([63, {"message": "Critical error."}]));
+    client.write(getBuffer([63, {"message": "Critical error."}]));
     client.end(async() => {
         console.info("Connection closed.");
         process.kill(process.pid);
