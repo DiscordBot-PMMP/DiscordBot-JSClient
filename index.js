fc4ba39d1bf6483cc71031e4484724db1ec32787
Jack Honour <JaxkDev@gmail.com>
2023-07-01 15:11:28 +0100

    Latest protocol testing

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function readPackets(data){
     let packetData = data.subarray(4, packetLength + 4);
 
     let id = packetData.readUInt16BE(0);
-    let uid = packetData.readUInt32BE(2);
-    packetData = packetData.subarray(6);
-    let packet = {"id": id, "uid": uid};
+    //let uid = packetData.readUInt32BE(2);
+    packetData = packetData.subarray(2);
+    let packet = {"id": id};//, "uid": uid};
 
     //manual packet deserialization for now.
     if(id === 0){
@@ -53,33 +53,33 @@ function writePacket(id, packet) {
     //manual packet serialization for now.
     let buf;
     if(id === 0){
-        buf = Buffer.alloc(4 + 2 + 4 + 1 + 4);
-        buf.writeUInt32BE(2 + 4 + 1 + 4); //packet length
+        buf = Buffer.alloc(4 + 2 + /*4 +*/ 1 + 4);
+        buf.writeUInt32BE(2 + /*4 +*/ 1 + 4); //packet length
         buf.writeUInt16BE(id, 4); //packet id
-        buf.writeUInt32BE(0, 4 + 2); //uid
-        buf.writeUInt8(packet.version, 4 + 2 + 4); //version
-        buf.writeUInt32BE(packet.magic, 4 + 2 + 4 + 1); //magic
+        //buf.writeUInt32BE(0, 4 + 2); //uid
+        buf.writeUInt8(packet.version, 4 + 2 /*+ 4*/); //version
+        buf.writeUInt32BE(packet.magic, 4 + 2 /*+ 4*/ + 1); //magic
         //Connect packet
     }else if(id === 1){
         //Heartbeat packet
-        buf = Buffer.alloc(4 + 2 + 4 + 4);
-        buf.writeUInt32BE(2 + 4 + 4); //packet length
+        buf = Buffer.alloc(4 + 2 + /*4 +*/ 4);
+        buf.writeUInt32BE(2 + /*4 +*/ 4); //packet length
         buf.writeUInt16BE(id, 4); //packet id
-        buf.writeUInt32BE(packet.uid, 4 + 2); //uid
-        buf.writeUInt32BE(packet.heartbeat, 4 + 2 + 4); //heartbeat
-
+        //buf.writeUInt32BE(packet.uid, 4 + 2); //uid
+        buf.writeUInt32BE(packet.heartbeat, 4 + 2 /*+ 4*/); //heartbeat
     }else if(id === 63){
         //Disconnect packet
-        buf = Buffer.alloc(4 + 2 + 4 + 4 + packet.message.length);
-        buf.writeUInt32BE(2 + 4 + 4 + packet.message.length); //packet length
+        buf = Buffer.alloc(4 + 2 + /*4 +*/ 4 + packet.message.length);
+        buf.writeUInt32BE(2 + /*4 +*/ 4 + packet.message.length); //packet length
         buf.writeUInt16BE(id, 4); //packet id
-        buf.writeUInt32BE(packet.uid, 4 + 2); //uid
-        buf.writeUInt32BE(packet.message.length, 4 + 2 + 4); //message length
-        buf.write(packet.message, 4 + 2 + 4 + 4); //message
+        //buf.writeUInt32BE(packet.uid, 4 + 2); //uid
+        buf.writeUInt32BE(packet.message.length, 4 + 2 /*+ 4*/); //message length
+        buf.write(packet.message, 4 + 2 /*+ 4*/ + 4); //message
     }else{
         console.error(`Unknown packet id ${id} to write.`);
         return;
     }
+    console.log(`Sending packet: ${JSON.stringify(packet)}`);
     client.write(buf);
 }
 
